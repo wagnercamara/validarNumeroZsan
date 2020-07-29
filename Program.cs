d9e33f9b764ce1ed03e9973caccb270cb574d81b
wagner <wagnercamara1994@gmail.com>
2020-07-29 18:51:23 -0300

    Start Teste

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace validarNumeroZsan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
